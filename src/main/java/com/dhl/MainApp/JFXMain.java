60df58eb80184dfefab1e6cc930946a4dbb032a7
LY <464742435@qq.com>
2023-02-04 22:17:22 +0800

    screen size

--- src/main/java/com/dhl/MainApp/JFXMain.java
+++ src/main/java/com/dhl/MainApp/JFXMain.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -14,9 +15,26 @@ public class JFXMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        String xmlName = "";
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int ScreenWidth = gd.getDisplayMode().getWidth();
+        int ScreenHeight = gd.getDisplayMode().getHeight();
+        int width = 0;
+        int height = 0;
+        if (ScreenWidth >= 1920){
+            System.out.println("big");
+            xmlName = "MainScreen.fxml";
+            width = 1522;
+            height = 818;
+        }else {
+            System.out.println("small");
+            xmlName = "MainScreen_Small.fxml";
+            width = 1186;
+            height = 730;
+        }
+        Parent root = FXMLLoader.load(getClass().getResource(xmlName));
         primaryStage.setTitle("DHL商品信息获取工具 Version.2.0.0.2");
-        primaryStage.setScene(new Scene(root, 1522, 818));
+        primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
 
 //        primaryStage.showAndWait();
